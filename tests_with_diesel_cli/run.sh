376c340f89a891e5801a589daa5d39944bf6eec6
Alex Whitney <adwhit@fastmail.com>
2023-02-14 17:40:13 +0000

    split diesel cli test into features

--- tests_with_diesel_cli/run.sh
+++ tests_with_diesel_cli/run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+set -xe
 
 export DATABASE_URL=postgres://postgres:postgres@localhost:5432
+
+rm -rf src/schema.rs
+rm -rf src/custom_schema.rs
 
 # create a 'default' schema
 docker-compose down
 docker-compose up -d
 sleep 1
+
 diesel migration run
+cargo test
+rm -rf src/schema.rs
+diesel migration revert
 
 # create a custom schema
-docker-compose down
-docker-compose up -d
-sleep 1
 diesel migration run --config-file custom.diesel.toml
-
-cargo test
+cargo test --features custom
+rm -rf src/custom_schema.rs
